105d999efbf1bdf4c2a669bdd51f74d9f7dc8275
jramirc <jramirc111@gmail.com>
2021-05-18 15:33:03 -0500

    Clase 2021-05-18 Data Annotations para validar campos de Registro

--- CursoMVC/Models/User/RegisterViewModel.cs
+++ CursoMVC/Models/User/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,32 @@ namespace CursoMVC.Models.User
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage ="El Campo Nombre es Obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage ="El Nombre solo debe contener LETRAS")]
         public string Nombre { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El Apellido solo debe contener LETRAS")]
         public string Apellido { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "La Cedula solo admite numeros")]
         public string Cedula { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Correo Invalido")]
         public string Email { get; set; }
-        public string Perfil { get; set; }
+
+        [Required]
+        [Display(Name ="Contraseña")]
+        [DataType(DataType.Password)]
+        [MinLength(5, ErrorMessage ="* Minimo 5 Caracteres")]
         public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirmar Contraseña")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Las contraseñas Ingresadas NO coinciden")]
         public string PasswordConfirm { get; set; }
     }
 }
